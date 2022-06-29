4d75cfbf0807938410304fc3ed9065cfa32f4aa4
xingchen <xingchen.zhao@huimeihealth.com.cn>
2022-06-29 21:23:58 +0800

    add login button

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { navs } from './config';
 import styles from './index.module.scss';
+import { Button } from 'antd';
 
 const Navbar: NextPage = () => {
   const { pathname } = useRouter();
@@ -19,6 +20,10 @@ const Navbar: NextPage = () => {
             </a>
           </Link>
         ))}
+      </section>
+      <section className={styles.operationArea}>
+        <Button>写文章</Button>
+        <Button>登录</Button>
       </section>
     </div>
   );
